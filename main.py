fd7dec040c0e2d95affa7024d614515d020c0ec4
Nitesh Ghimire <niteshghimire223@gmail.com>
2025-11-26 08:26:18 +0545

    add dockerfile

--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 from fastapi import FastAPI
 from routes import text_gen
 from routes import img_gen
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods = ['*'],
+    allow_headers = ['*'],
+    allow_origins = ['*']
+)
 
 app.include_router(text_gen.router)
 app.include_router(img_gen.router)
